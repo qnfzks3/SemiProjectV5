63dd6b4fdea42c4088bd030a3dcd4d6769d8b408
Lea Sae Cheul <dltpcjf1005@naver.com>
2023-05-11 15:58:59 +0900

    mvc 프로젝트 - model - lombok 설정

--- src/main/java/qnfzks3/springboot/semiprojectv5/model/Zipcode.java
+++ src/main/java/qnfzks3/springboot/semiprojectv5/model/Zipcode.java
@@ -1,5 +1,8 @@
 package qnfzks3.springboot.semiprojectv5.model;
 
+import lombok.Data;
+
+@Data
 public class Zipcode {
 
     private String zipcode;
@@ -9,54 +12,5 @@ public class Zipcode {
     private String ri;
     private String bunji;
 
-    public Zipcode() {
-    }
 
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
-
-    public String getSido() {
-        return sido;
-    }
-
-    public void setSido(String sido) {
-        this.sido = sido;
-    }
-
-    public String getGugun() {
-        return gugun;
-    }
-
-    public void setGugun(String gugun) {
-        this.gugun = gugun;
-    }
-
-    public String getDong() {
-        return dong;
-    }
-
-    public void setDong(String dong) {
-        this.dong = dong;
-    }
-
-    public String getRi() {
-        return ri;
-    }
-
-    public void setRi(String ri) {
-        this.ri = ri;
-    }
-
-    public String getBunji() {
-        return bunji;
-    }
-
-    public void setBunji(String bunji) {
-        this.bunji = bunji;
-    }
 }
