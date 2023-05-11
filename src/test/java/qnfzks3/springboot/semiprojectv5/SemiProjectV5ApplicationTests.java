96340ccd4e471518b40a2053d8a44a8b4201b9b3
Lea Sae Cheul <dltpcjf1005@naver.com>
2023-05-11 12:57:53 +0900

    저장소 초기화 - boot 프로젝트 설정

---
+++ src/test/java/qnfzks3/springboot/semiprojectv5/SemiProjectV5ApplicationTests.java
@@ -0,0 +1,13 @@
+package qnfzks3.springboot.semiprojectv5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemiProjectV5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
